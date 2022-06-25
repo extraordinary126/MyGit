7fdaacba9d6253b920536de508fac699a4ffecc7
extraordinary126 <95428052+extraordinary126@users.noreply.github.com>
2022-06-25 16:29:33 +0800

    haha

--- src/com/yuhao/git/hello1.java
+++ src/com/yuhao/git/hello1.java
@@ -2,7 +2,7 @@ package src.com.yuhao.git;
 
 public class hello1 {
     public static void main(String[] args) {
-
+        System.out.println("");
         System.out.println("没有添加到暂存区");
     }
 }
